9bdff825835d2a62209f33f1974fc3d6ede2f247
Umar Balak <umarbalak35@gmail.com>
2024-03-14 15:59:02 +0530

    updated threshold for face verification

--- cv_models/face_verification/main.py
+++ cv_models/face_verification/main.py
@@ -1,11 +1,23 @@
+import cv2
+import time
 from functions import *
+
 camera = cv2.VideoCapture(0)
-while True:
+start_time = time.time()
+
+while time.time() - start_time < 180:
+    # print(time.time() - start_time)  # Run for 180 seconds
     ret, frame = camera.read()
     verification = verify(frame)
-    if verification is True:
+    if verification:
         print("Candidate Verified.")
         break
     else:
         continue
 
+# Release the camera
+camera.release()
+
+# If 30 seconds have elapsed and verification is not successful, print "False"
+if not verification:
+    print("Unable to verify.")
