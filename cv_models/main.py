a5ad993c0984e13ca04fcbdf043b1641122bcd58
Umar Balak <umarbalak35@gmail.com>
2024-03-14 17:42:14 +0530

    fix bugs

--- cv_models/main.py
+++ cv_models/main.py
@@ -1,11 +1,10 @@
 from functions import *
+camera = cv2.VideoCapture(0)
 while True:
     output = run()
     # if everything goes well, returns a python dictionary-->
     #    {"direction": direction, "distance": distance}
     # else returns a python dictionary-->
     #    {"face_detected": False}
-    if output["termination"] is True:
-        print(output["result"])
-        break
-    print(output["result"])
+
+    print(output)
