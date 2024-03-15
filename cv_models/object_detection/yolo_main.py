46a71e4074d931e2305dc52b3fe8ec80d3809156
Umar Balak <umarbalak35@gmail.com>
2024-03-15 08:01:18 +0530

    change the return value

--- cv_models/object_detection/yolo_main.py
+++ cv_models/object_detection/yolo_main.py
@@ -3,5 +3,5 @@ from yolo_functions import *
 camera = cv2.VideoCapture(0)
 
 while True:
-    count = obj_detect(camera)
-    print(count)
+    count, alert = obj_detect(camera)
+    print(count, alert)   
