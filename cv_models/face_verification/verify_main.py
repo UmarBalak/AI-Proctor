624b0e45291db90d0fb8499a0af70da63ff3b78f
Umar Balak <umarbalak35@gmail.com>
2024-03-16 12:55:41 +0530

    new model

--- cv_models/face_verification/verify_main.py
+++ cv_models/face_verification/verify_main.py
@@ -11,13 +11,16 @@ while time.time() - start_time < 180:
     count = 0
     for i in frame:
         count += len(i) 
-    print(len(frame))
-    print(count)
     verification = verify(frame)
     if verification:
         flag = True
         break
     else:
+        print("Verifying...")
         continue
+if flag:
+    print("verified")
+else:
+    print("Not verified")
 # Release the camera
 camera.release()
